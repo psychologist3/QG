24185710fc8674c90082d72ec9c00efb9338f425
psychologist3 <163643428+psychologist3@users.noreply.github.com>
2024-03-17 17:11:19 +0800

    Delete 1 directory

--- 1/Statement.h
+++
@@ -1,6 +0,0 @@
-void createList(struct list** head);
-void destoryList(struct list *head);
-struct list* insertNode(struct list *head);
-void  seeknode(struct list*head);
-struct list *deleteNode(struct list *head);
-struct list * exchange(struct list*head);
--- 1/StructList.h
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-//定义一个结构体
-struct list{
-    int number;
-    struct list *next;
-};
