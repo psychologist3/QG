0cfe7b8cccf7ca818551c9eb3ea810e1b8f56875
psychologist3 <163643428+psychologist3@users.noreply.github.com>
2024-03-21 20:32:43 +0800

    Add files via upload

---
+++ 小组/Week1/jdbc-demo/src/com/QG/JDBCTest.java
@@ -0,0 +1,12 @@
+package com.QG;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCTest {
+    public static void main(String[] args) throws Exception {
+        JDBCUtils.SQL("drop database db2;");
+    }
+}
